a5a5062b8d8c3c2625bd0b07f0eb4f9f83f32e69
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-04-03 16:28:41 -0500

    m

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,11 +5,18 @@ import java.util.LinkedList;
 import controller.ControllerModel;
 import model.Pelicula;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
 	
 	private ControllerModel controller;
-	
+	PImage breakingbad;
+	PImage dark;
+	PImage mindhunter;
+	PImage ozark;
+	PImage startrek;
+	PImage strangerthings;
+	PImage theirishman;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
